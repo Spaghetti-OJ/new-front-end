8fbc128ce3db2c35b3152bcc318e35d03e0e7afc
cyucccx <yuyuchen2005@gmail.com>
2025-10-15 02:45:18 +0800

    chore: prittier code style

--- src/api/problems.ts
+++ src/api/problems.ts
@@ -8,10 +8,11 @@ export const Problem = {
     fetcher.put(`/problem/manage/${id}`, body, { headers: { "Content-Type": "multipart/form-data" } }),
   delete: (id: string | number) => fetcher.delete(`/problem/manage/${id}`),
 
-  initiateTestCaseUpload: (
-    problemId: number,
-    body: { length: number; partSize: number },
-  ) => fetcher.post<{ upload_id: string; urls: string[] }>(`/problem/${problemId}/initiate-test-case-upload`, body),
+  initiateTestCaseUpload: (problemId: number, body: { length: number; partSize: number }) =>
+    fetcher.post<{ upload_id: string; urls: string[] }>(
+      `/problem/${problemId}/initiate-test-case-upload`,
+      body,
+    ),
 
   completeTestCaseUpload: (
     problemId: number,
