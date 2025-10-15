dce4975596aa141b7fbb617dcf24626c3279fa6d
cyucccx <yuyuchen2005@gmail.com>
2025-10-16 00:04:18 +0800

    chore: rename api related filenames

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,17 +1,17 @@
-import { Auth, Copycat } from "./auths";
-import { Problem } from "./problems";
-import { Submission } from "./submissions";
-import { Assignments, Homework } from "./assignments";
-import { Announcement, Course } from "./courses";
+import { Auth, Copycat } from "./auth";
+import { Problem } from "./problem";
+import { Submission } from "./submission";
+import { Assignments, Homework } from "./homework";
+import { Announcement, Course } from "./course";
 import { User } from "./user";
 import { AIVTuber } from "./aivtuber";
 
 export { fetcher } from "./fetcher";
-export { Auth, Copycat } from "./auths";
-export { Problem } from "./problems";
-export { Submission } from "./submissions";
-export { Assignments, Homework } from "./assignments";
-export { Announcement, Course } from "./courses";
+export { Auth, Copycat } from "./auth";
+export { Problem } from "./problem";
+export { Submission } from "./submission";
+export { Assignments, Homework } from "./homework";
+export { Announcement, Course } from "./course";
 export { User } from "./user";
 export { AIVTuber } from "./aivtuber";
 
