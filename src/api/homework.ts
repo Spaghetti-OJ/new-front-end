af618683e247e03f62896c1877777d1c1fd81a1d
cyucccx <yuyuchen2005@gmail.com>
2025-10-16 00:16:49 +0800

    chore: delete Assignments in api module

--- src/api/homework.ts
+++ src/api/homework.ts
@@ -1,10 +1,9 @@
 import { fetcher } from "./fetcher";
 
-const assignments = {
+const homework = {
   create: (body: HomeworkCreationForm) => fetcher.post("/homework", body),
   modify: (id: string, body: HomeworkEditionForm) => fetcher.put(`/homework/${id}`, body),
   delete: (id: string) => fetcher.delete(`/homework/${id}`),
 };
 
-export const Assignments = assignments;
-export const Homework = assignments;
+export const Homework = homework;
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import { Auth, Copycat } from "./auth";
 import { Problem } from "./problem";
 import { Submission } from "./submission";
-import { Assignments, Homework } from "./homework";
+import { Homework } from "./homework";
 import { Announcement, Course } from "./course";
 import { User } from "./user";
 import { AIVTuber } from "./aivtuber";
@@ -10,7 +10,7 @@ export { fetcher } from "./fetcher";
 export { Auth, Copycat } from "./auth";
 export { Problem } from "./problem";
 export { Submission } from "./submission";
-export { Assignments, Homework } from "./homework";
+export { Homework } from "./homework";
 export { Announcement, Course } from "./course";
 export { User } from "./user";
 export { AIVTuber } from "./aivtuber";
@@ -19,7 +19,6 @@ const api = {
   Auth,
   Problem,
   Submission,
-  Assignments,
   Homework,
   Announcement,
   Course,
