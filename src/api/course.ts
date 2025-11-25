01ffc4c31d785c888fe9048848d6479152ee9d16
cyucccx <yuyuchen2005@gmail.com>
2025-11-26 02:37:53 +0800

    fix: create course api endpoint

--- src/api/course.ts
+++ src/api/course.ts
@@ -1,7 +1,7 @@
 import { fetcher } from "./fetcher";
 
 export const Course = {
-  create: (body: CourseForm) => fetcher.post("/course", body),
+  create: (body: CourseForm) => fetcher.post("/course/", body),
   list: () => fetcher.get<{ courses: CourseList }>("/course/"),
 };
 
