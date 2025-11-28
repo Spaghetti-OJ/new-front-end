a29ac1465c4277ed1c0a99fa2cfde019e256fc74
cyucccx <yuyuchen2005@gmail.com>
2025-11-28 09:33:22 +0800

    feat: add teacher can access course member list

--- src/components/Course/CourseSideBar.vue
+++ src/components/Course/CourseSideBar.vue
@@ -26,32 +26,26 @@ const navs = [
     name: t("components.courseSideBar.submissions"),
     path: "/submissions",
   },
-  ...(session.isAdmin
+  ...(session.isAdmin || session.role === "teacher"
     ? [
-        {
-          name: t("components.courseSideBar.members"),
-          path: "/members",
-        },
-      ]
+      {
+        name: t("components.courseSideBar.members"),
+        path: "/members",
+      },
+    ]
     : []),
 ];
 </script>
 
 <template>
   <!-- 僅在 /courses/[name]/... 底下顯示側邊/分頁導覽 -->
-  <ul
-    v-if="displayType === 'side' && $route.params.name"
-    class="menu menu-compact w-40 bg-base-100 p-2 lg:menu-normal"
-  >
-    <li
-      v-for="{ name, path } in navs"
-      :class="[
-        {
-          'border-l-4 border-blue-500':
-            $route.params.name && $route.path === `/courses/${$route.params.name}${path}`,
-        },
-      ]"
-    >
+  <ul v-if="displayType === 'side' && $route.params.name" class="menu menu-compact w-40 bg-base-100 p-2 lg:menu-normal">
+    <li v-for="{ name, path } in navs" :class="[
+      {
+        'border-l-4 border-blue-500':
+          $route.params.name && $route.path === `/courses/${$route.params.name}${path}`,
+      },
+    ]">
       <router-link :to="`/courses/${$route.params.name}${path}`">{{ name }}</router-link>
     </li>
   </ul>
@@ -59,12 +53,9 @@ const navs = [
   <div v-else-if="$route.params.name" class="scrollbar-hide w-full overflow-scroll">
     <div class="tabs mx-auto w-max">
       <template v-for="{ name, path } in navs">
-        <a
-          class="tab tab-bordered h-10 w-32"
-          :class="{
-            'tab-active': $route.params.name && $route.path === `/courses/${$route.params.name}${path}`,
-          }"
-        >
+        <a class="tab tab-bordered h-10 w-32" :class="{
+          'tab-active': $route.params.name && $route.path === `/courses/${$route.params.name}${path}`,
+        }">
           <router-link :to="`/courses/${$route.params.name}${path}`">{{ name }}</router-link>
         </a>
       </template>
