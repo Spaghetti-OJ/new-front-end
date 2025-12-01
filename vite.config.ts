7a94a1d11ef39d17cdd41e4c374988948efcae10
cyucccx <yuyuchen2005@gmail.com>
2025-12-02 01:14:52 +0800

    chore: update API server target URL in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.NODE_ENV === "test" ? "http://localhost:8000" : "https://noj-preview1.little7.pp.ua",
+        target: process.env.NODE_ENV === "test" ? "http://localhost:8000" : "https://noj-backend-dev.little7.pp.ua",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
