8593e061dd2b762f78d7f04d849a846a7b8a6272
cyucccx <yuyuchen2005@gmail.com>
2025-12-28 05:52:29 +0800

    fix: enhance homework problem display with improved problem metadata … (#116)
    * fix: enhance homework problem display with improved problem metadata handling
    
    * fix: update homework prop types and enhance problem metadata handling
    
    * fix: improve problem metadata fetching with enhanced data validation
    
    * fix: add optional highScore property to ProblemItem interface
    
    * fix: enhance isQuotaUnlimited function to handle null and undefined values

--- src/pages/courses/[courseId]/homeworks/index.vue
+++ src/pages/courses/[courseId]/homeworks/index.vue
@@ -4,7 +4,6 @@ import api from "@/api";
 import { useSession } from "@/stores/session";
 import { useTitle } from "@vueuse/core";
 import { computed, ref, onMounted } from "vue";
-import { useProblemSelection } from "@/composables/useProblemSelection";
 
 const session = useSession();
 const route = useRoute();
@@ -13,12 +12,6 @@ useTitle(`Homeworks - ${route.params.courseId} | Normal OJ`);
 const homeworksData = ref<GetHomeworksResponse>();
 const isLoading = ref(true);
 const error = ref<any>(null);
-
-const {
-  problemId2Meta,
-  error: fetchProblemError,
-  isLoading: isFetchingProblem,
-} = useProblemSelection(route.params.courseId as string);
 
 const homeworks = computed(() => {
   if (!homeworksData.value) return [];
@@ -55,7 +48,7 @@ onMounted(async () => {
             <i-uil-plus-circle class="mr-1 lg:h-5 lg:w-5" /> {{ $t("course.hw.index.new") }}
           </router-link>
         </div>
-        <data-status-wrapper :error="error || fetchProblemError" :is-loading="isLoading || isFetchingProblem">
+        <data-status-wrapper :error="error" :is-loading="isLoading">
           <template #loading>
             <skeleton-card />
           </template>
@@ -64,7 +57,6 @@ onMounted(async () => {
               v-for="homework in homeworks"
               :key="homework.id"
               :homework="homework"
-              :problems="problemId2Meta"
               class="mb-2"
             />
           </template>
